e2e830db553196fc60e6744415af155294426826
gameupyourlife <cedric.balzer02@gmail.com>
2023-11-19 21:13:42 +0100

    Test

--- utils/dbConnection.ts
+++ utils/dbConnection.ts
@@ -25,7 +25,7 @@ async function getLocalJsonData(): Promise<any[]> {
             });
         }
         else {
-            fs.readFile("./playersDB.json", 'utf8', (err, data) => {
+            fs.readFile("public/playersDB.json", 'utf8', (err, data) => {
                 if (err) {
                     // console.error(err);
                     reject(err);
