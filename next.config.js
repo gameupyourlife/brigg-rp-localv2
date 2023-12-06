41758947997bcd1e035652412ea47c2b1219e448
gameupyourlife <cedric.balzer02@gmail.com>
2023-12-06 14:28:07 +0100

    Config fix

--- next.config.js
+++ next.config.js
@@ -29,25 +29,4 @@ const nextConfig = {
 //     //         ],
 //     //     })
 
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.md$/,
-      use: [
-        {
-          loader: "html-loader",
-        },
-        {
-          loader: "markdown-loader",
-          options: {
-            // Pass options to marked
-            // See https://marked.js.org/using_advanced#options
-          },
-        },
-      ],
-    })
-
-    return config
-  },
-}
-
 module.exports = nextConfig
