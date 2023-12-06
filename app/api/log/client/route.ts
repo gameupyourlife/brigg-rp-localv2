3ed4ce531d439f0126df622c2a3ec683066ce354
gameupyourlife <cedric.balzer02@gmail.com>
2023-12-06 14:39:25 +0100

    Changed res

--- app/api/log/client/route.ts
+++ app/api/log/client/route.ts
@@ -3,8 +3,9 @@ import { logClientData } from "@/utils/logging";
 export async function POST(request: Request, response: Response) {
     // console.log(request.json());
     // logClientData(res);
-    const res = await request.json()
-    logClientData({ data: res });
-    return Response.json({ res })
+    response = await request.json()
+    logClientData({ data: response });
+    return response.json()
+
     // return Response  
 }
